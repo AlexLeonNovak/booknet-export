514f7762bc981fc13eaf9e8634a29a98016091ca
Alex Novak <alexleonnovak@gmail.com>
2022-09-20 14:25:19 +0300

    create user without email

--- data.adapter.js
+++ data.adapter.js
@@ -12,7 +12,7 @@ export const toMauticData = (fields) => ({
 	confirmation: fields['Consent'],
 	mobile: fields['Mobile'],
 	cansendsms: fields['Could_send_sms'],
-	email: fields['Email'],
+	email: fields['Email'] || fields['Customer_Id'] + '@email.com',
 	cansendemail: fields['Could_send_email'],
 	city: fields['City'],
 	preferredbranch12m: fields['preferredBranch_12mon'],
